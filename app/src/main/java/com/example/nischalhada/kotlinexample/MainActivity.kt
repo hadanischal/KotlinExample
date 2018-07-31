acdec2614d10b5733161e3eac2f2c22f6356a403
Nischal Hada <hadanischal@gmail.com>
2018-07-31 12:44:17 +1000

    implement simple challenge

--- app/src/main/java/com/example/nischalhada/kotlinexample/MainActivity.kt
+++ app/src/main/java/com/example/nischalhada/kotlinexample/MainActivity.kt
@@ -8,5 +8,15 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        runChallenge()
     }
+
+    private fun runChallenge() {
+        val names = listOf("Schultz", "Cuevas", "Rhodes", "Mckenzie", "Taylor", "Glenn", "Reilly", "Morris", "Herman", "Beltran", "Swanson", "Roth")
+        val challenge = Challenge()
+        val result = challenge.process(names)
+
+        println(result)
+    }
+
 }
