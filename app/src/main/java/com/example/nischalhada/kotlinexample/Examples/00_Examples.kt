1832e1fe5715dc8247856b046a3c69b9302e7f9c
Nischal Hada <hadanischal@gmail.com>
2018-08-02 09:02:42 +1000

    implement the methods to be added

--- app/src/main/java/com/example/nischalhada/kotlinexample/Examples/00_Examples.kt
+++ app/src/main/java/com/example/nischalhada/kotlinexample/Examples/00_Examples.kt
@@ -1,2 +1,54 @@
 package com.example.nischalhada.kotlinexample.Examples
 
+object Examples {
+    fun run() {
+        runLambdaExamples()
+        runExceptionExamples()
+        runInterfaceExamples()
+        runInheritanceExamples()
+        runEnumClassesExamples()
+        runGenericsExamples()
+        runExtensionExamples()
+        runJavaInteropExamples()
+    }
+}
+
+//region Examples
+
+private fun runLambdaExamples(){
+
+}
+
+private fun runExceptionExamples() {
+
+}
+
+private fun runInterfaceExamples() {
+
+}
+
+private fun runInheritanceExamples() {
+}
+
+private fun runEnumClassesExamples() {
+
+}
+
+
+private fun runGenericsExamples() {
+
+}
+
+private fun runExtensionExamples() {
+
+}
+
+private fun runJavaInteropExamples() {
+
+}
+
+//endregion
+
+//region helper methods
+fun authenticate(result: String): Boolean = result == "Naruto Uzumaki"
+//endregion
