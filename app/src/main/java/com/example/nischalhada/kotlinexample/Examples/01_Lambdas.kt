b8aec684f15c4f4887d963e1d0c95d052822eb48
Nischal Hada <hadanischal@gmail.com>
2018-08-05 22:00:18 +1000

    implemented async network call

--- app/src/main/java/com/example/nischalhada/kotlinexample/Examples/01_Lambdas.kt
+++ app/src/main/java/com/example/nischalhada/kotlinexample/Examples/01_Lambdas.kt
@@ -1,2 +1,17 @@
 package com.example.nischalhada.kotlinexample.Examples
 
+import com.example.nischalhada.kotlinexample.Utilities.Threading
+
+object Lambdas {
+    fun asyncNetworkCall(finished: (Boolean)->Unit) {
+        Threading.async(task = {
+            println("asyncNetworkCall starting...")
+            Thread.sleep(2000)
+            println("asyncNetworkCall finished")
+
+            finished(true)
+        })
+    }
+
+
+}
